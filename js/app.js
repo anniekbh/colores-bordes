a3280dd88d88612457f646140db58823c1864164
anniekbh <anniekbh@gmail.com>
2016-09-15 17:40:40 -0500

    creando proyecto

---
+++ js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+    var boton = document.getElementById("cri");
+        boton.addEventListener("click", function(){
+            document.getElementById("circuloUno").innerHTML= "hola";
+           
+    });  
+    
+});
